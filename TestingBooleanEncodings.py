47997bce3b33aaf013de5e98d719bc486313b6ca
Archan Patkar <archanpatkar@gmail.com>
2017-12-26 02:20:09 +0530

    Create TestingBooleanEncodings.py

---
+++ TestingBooleanEncodings.py
@@ -0,0 +1,21 @@
+from BooleanEncoding import *
+
+print("BOOLS");
+print(TRUE);
+print(FALSE);
+
+print("NOT Permutation");
+print(NOT(TRUE));
+print(NOT(FALSE));
+
+print("AND Permutation");
+print(AND(TRUE)(TRUE));
+print(AND(TRUE)(FALSE));
+print(AND(FALSE)(FALSE));
+print(AND(FALSE)(TRUE));
+
+print("OR Permutation");
+print(OR(TRUE)(TRUE));
+print(OR(TRUE)(FALSE));
+print(OR(FALSE)(FALSE));
+print(OR(FALSE)(TRUE));
