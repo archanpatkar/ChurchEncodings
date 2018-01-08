f433f73dc6a8c1970e38a976b8ccf019234253d0
Archan Patkar <archanpatkar@gmail.com>
2018-01-08 23:55:03 +0530

    Update Datastructures.py

--- Datastructures/Datastructures.py
+++ Datastructures/Datastructures.py
@@ -18,3 +18,27 @@ leq = lambda n: lambda k: iszero(sub(n)(k));
 eq = lambda n: lambda k: AND(leq(n)(k))(leq(k)(n));
 
 geq = lambda n: lambda k: blackbird(NOT)(leq)(n)(k);
+
+p1 = pair(zero)(zero);
+
+print(num(first(p1)));
+
+print(num(second(p1)));
+
+p2 = phi(p1);
+
+print(num(first(p2)));
+
+print(num(second(p2)));
+
+print(num(pred(thrice)));
+
+print(num(sub(thrice)(twice)));
+
+print(leq(twice)(thrice));
+
+print(eq(twice)(twice));
+
+print(eq(zero)(twice));
+
+print(geq(thrice)(twice));
