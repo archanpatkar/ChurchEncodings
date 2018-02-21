e524f61f10b688b41c02376dd8b2e350d03b4d5b
Archan Patkar <archanpatkar@gmail.com>
2018-02-21 16:52:48 +0530

    Update Booleans.py

--- Boolean/Booleans.py
+++ Boolean/Booleans.py
@@ -4,8 +4,8 @@ FALSE lambda x: lambda y: y;
 
 NOT = lambda b: b(FALSE)(TRUE);
 
-AND lambda x: lambda y: x(y)(x);
+AND = lambda x: lambda y: x(y)(x);
 
-OR lambda x: lambda y: x(x)(y);
+OR = lambda x: lambda y: x(x)(y);
 
 # XOR = lambda x: lambda y:
